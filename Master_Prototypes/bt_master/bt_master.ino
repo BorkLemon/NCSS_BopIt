01485d3e465dbe3613e1c5a7b481f9b3213d0461
aquaticpolarbear <acarter010204@gmail.com>
2015-01-11 15:14:20 +1100

    added the first revision of the bt_master file

---
+++ Master_Prototypes/bt_master/bt_master.ino
@@ -0,0 +1,28 @@
+#include <SoftwareSerial.h>
+
+const int RxD = 6;
+const int TxD = 7;
+
+SoftwareSerial btSerial(RxD, TxD);
+
+void setup(){
+  Serial.begin(9600);
+  btSerial.begin(38400);
+ pinMode(RxD, INPUT);
+ pinMode(TxD, OUTPUT);
+}
+
+void loop(){
+    if(btSerial.available()) {
+       while(btSerial.available()) {
+          Serial.write(btSerial.read());
+          delay(10);
+       } 
+    }
+    if(Serial.available()) {
+      while(Serial.available()) {
+        btSerial.write(Serial.read());
+        delay(10);
+      }
+    }
+}
