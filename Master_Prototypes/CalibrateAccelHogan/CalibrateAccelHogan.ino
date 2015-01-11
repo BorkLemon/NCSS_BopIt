806e7a244d2f6523080d220027b1109645b25947
ahog5379 <ahog5379@wfgx0xx1.internal.usyd.edu.au>
2015-01-11 15:32:34 +1100

    Hogan did some cool stuff and calibration works!

--- Master_Prototypes/CalibrateAccelHogan/CalibrateAccelHogan.ino
+++ Master_Prototypes/CalibrateAccelHogan/CalibrateAccelHogan.ino
@@ -18,32 +18,32 @@ void Cal(){ //Calibration function - no return because of global variables.
   float sumX= 0.0; //Arrays for averaging calibration values for X, Y and Z 
   float sumY= 0.0;
   float sumZ= 0.0;
-  for (int i=0; i<SAMPLES - 1; i++){
+  for (int i=0; i<SAMPLES; i++){
     XValue = analogRead(A0); //read the Analogue inputs
     YValue = analogRead(A1);
     ZValue = analogRead(A2);
     xV = XValue*3.3/1023; //convert to voltages
+
     yV = YValue*3.3/1023;
     zV = ZValue*3.3/1023;
     xG = xV*1.5/0.8; //convert to Gravities
     yG = yV*1.5/0.8;
-    zG = zV*1.5/0.8;   
+    zG = zV*1.5/0.8-1;   
     sumX+=xG;
     sumY+=yG;
     sumZ+=zG;
   xCal = sumX/SAMPLES; //Calibration values for X Y and Z
   yCal = sumY/SAMPLES;
   zCal = sumZ/SAMPLES;
-  
+  delay(50);
   }
+  
 }
 
 void setup(){
   Serial.begin(9600); //Turn on the Serial Bus for the Srial Monitor
-  delay(1000); //delay to pass by the spike in voltages on pins at startup
+  delay(2000); //delay to pass by the spike in voltages on pins at startup
   Cal();
-  
-
 }
 
 void loop (){
@@ -54,7 +54,7 @@ void loop (){
   yV = YValue*3.3/1023;
   zV = ZValue*3.3/1023;
   xG = xV*1.5/0.8;
-  yG = yV*1.5/0.8;
+  yG = yV*1.5/0.8;f
   zG = zV*1.5/0.8;  
   
   Serial.print("x - ");
