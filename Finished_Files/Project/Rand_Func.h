39e628ffd5461f262377a4f0fd2f9fb0441923e5
unknown <kgil6664@W4X3JG2S.internal.usyd.edu.au>
2015-01-12 11:30:50 +1100

    Integrated Rand_Func class (random function) into main project

---
+++ Finished_Files/Project/Rand_Func.h
@@ -0,0 +1,7 @@
+#ifndef _Rand_Func_h
+#define _Rand_Func_h
+
+// returns a random number between 1 and num
+int randomise(float num);
+
+#endif
---
+++ Finished_Files/Project/Rand_Func.ino
@@ -0,0 +1,7 @@
+#include "Rand_Func.h"
+
+int randomise(float num){
+  return millis() % num;
+}
+
+
