262337b7ecad1bab2b0bbf8727fcf1ed4d3b6be5
unknown <kgil6664@W4X3JG2S.internal.usyd.edu.au>
2015-01-12 11:22:49 +1100

    Tested Jump() and Punch() functions, both use y axis and are operational

--- Finished_Files/Project/Project.ino
+++ Finished_Files/Project/Project.ino
@@ -4,14 +4,14 @@
 
 void setup() {
   Serial.begin(9600);
-  bluetoothSetup();    // RELIES ON BLUETOOTH CODE
+  //bluetoothSetup();    // RELIES ON BLUETOOTH CODE
   calibrationSetup();  // RELIES ON CALIBRATION CODE
 }
 
 void loop() {
   // Checks if the player is punching or jumping and moves the robot forward
   if(jump() || punch()) {
-    btSerial.write(ROBOT_FORWARD); // RELIES ON BLUETOOTH CODE
+    //btSerial.write(ROBOT_FORWARD); // RELIES ON BLUETOOTH CODE
     Serial.println("check");
   }
 }
