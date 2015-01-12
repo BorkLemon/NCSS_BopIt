a837c6098610c479ebde17a89f06f4605814c2f6
unknown <kgil6664@WGX3JG2S.internal.usyd.edu.au>
2015-01-12 14:47:39 +1100

    WORKING VERSION FOR MAIN PROJECT FOLDER

---
+++ Finished_Files/Project - Back up (DO NOT EDIT)/Accelerometer_Auto_Calibrated.h
@@ -0,0 +1,22 @@
+#ifndef _Accelerometer_Auto_Calibrated_h
+#define _Accelerometer_Auto_Calibrated_h
+
+//Sets number of accelerometer readings to 10
+int NUM_Samples = 10;  
+float xCorrection;
+float yCorrection;
+float zCorrection;
+
+//Calulate float average - general
+float averages(int cals[]);
+
+// autocalibrate the accelerometer
+void calibration ();
+
+// initialised calibration
+void calibrationSetup();
+ 
+// Test calibration
+void debug();
+ 
+#endif
---
+++ Finished_Files/Project - Back up (DO NOT EDIT)/Actions.ino
@@ -0,0 +1,89 @@
+#include "Actions.h"
+#include "Accelerometer_Auto_Calibrated.h"
+
+// Detects an arm raising action (NON_FUNCTIONAL)
+bool armRaise(){ //Returns T or F based on whether the movement sent was a valid bow
+  
+  float geesY = readGees(ACCEL_PIN_Y, yCorrection);
+  float geesZ = readGees(ACCEL_PIN_Z, zCorrection);
+  
+  Serial.println(geesY);
+  Serial.print(",");
+  Serial.print(geesZ);
+  
+  // Detect if the arm is in the starting position
+  if (!armRaiseStart && (geesY > geesZ)){
+    armRaiseStart = true;
+      //  IMPLEMENT LATER
+//  tone(BUZZPIN,400,500);
+    //Serial.println("Hooray - start");
+  }
+  
+  // Check if the arm is in the arm raised position
+  else if(armRaiseStart && (geesZ > geesY)){
+    // Serial.println("Hooray - finish");
+    armRaiseStart = false;
+    return true;
+  }
+  
+  // Arm raise not detected
+  return false;
+}
+
+// Detects a jumping action (FUNCTIONAL)
+bool jump() {
+  
+  // get accelerometer value from z axis
+  float geesY = readGees(ACCEL_PIN_Y, yCorrection); // Zave DEPENDENT ON CALIBRATION CODE
+  // Detects a spike in the Z axis as a jumping action
+  
+  if (geesY > JUMP_SPIKE_HIGH) {
+    if (jumpLow) {
+      Serial.println("JUMP DETECTED");
+      jumpLow = false;
+      return true;
+    }
+    else
+      jumpSpike = true;
+  }
+  
+  // Detects a drop in the Z axis to confirm the end of the jump
+  if (geesY < JUMP_SPIKE_LOW && jumpSpike) {
+      jumpSpike = false;
+      jumpLow = true;
+  } 
+  // Occurs when a jump is not detected
+  return false;
+}
+
+
+// Detects a 'punching' action (FUNCTIONAL)
+bool punch() {
+  // TODO (OPTIONAL) Add policing for backward punches
+  
+  // load accelerometer gees from x axis
+  float geesY = readGees(ACCEL_PIN_Y, yCorrection); // Xave DEPENDENT ON CALIBRATION CODE
+
+  // Detects a spike in the x axis as a punching action
+  if (geesY > PUNCH_SPIKE_HIGH) {
+    punchSpike = true;
+  }
+  
+  // Detects a dip in the x axis to confirm the end of the punching action
+  if (geesY < PUNCH_SPIKE_LOW && punchSpike) {
+      Serial.println("PUNCH DETECTED");
+      punchSpike = false;
+      return true;
+  } 
+  
+  // Occurs when a punch is not detected
+  return false;
+}
+
+// Read analog input from accelerometer in gees
+float readGees(int pin, int correction) {
+   int input = analogRead(pin) - correction; // ADDED CORRECTION CODE FROM CALLIBRATION
+   float volts = (input/1023.0)*3.3;
+   float gees = ((volts*1.5)/0.8);
+   return gees;
+}
